33c5ea91bcd4fddf0b719145f34fa8687a5f5d7c
Yoltic2 <eyolticmg@icloud.com>
2022-01-20 19:24:41 -0600

    Initial Commit

---
+++ Calcula_Tu_Ibm/Calcula_Tu_IbmApp.swift
@@ -0,0 +1,17 @@
+//
+//  Calcula_Tu_IbmApp.swift
+//  Calcula_Tu_Ibm
+//
+//  Created by Emilio Y Martinez on 20/01/22.
+//
+
+import SwiftUI
+
+@main
+struct Calcula_Tu_IbmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
