c6be06e97f3c93bff66ae7f8c61227f2a73748f6
Yoltic2 <eyolticmg@icloud.com>
2022-01-20 21:25:45 -0600

    first version

---
+++ Calcula_Tu_Ibm/CalculadoVista.swift
@@ -0,0 +1,23 @@
+//
+//  CalculadoVista'.swift
+//  Calcula_Tu_Ibm
+//
+//  Created by Emilio Y Martinez on 20/01/22.
+//
+
+import SwiftUI
+
+struct CalculadoVista: View {
+    var height: Double
+    var width:Double
+    var body: some View {
+        let result = width/(pow(height, 2))
+        Text("\(String(format: "%.2f", result))").font(.title).bold().foregroundColor(.green)
+    }
+}
+
+struct CalculadoVista_Previews: PreviewProvider {
+    static var previews: some View {
+        CalculadoVista(height:10,width: 10)
+    }
+}
--- Calcula_Tu_Ibm/ContentView.swift
+++ Calcula_Tu_Ibm/ContentView.swift
@@ -8,11 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var height : Double = 0
+    @State var weight : Double = 0
+    @State var calculate = false
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+            VStack{
+                Text("Calculate Your BMI").font(.title).padding()
+                Spacer()
+                VStack(alignment:.leading){
+                    HStack{
+                        Text("Height").padding()
+                        Text("\(String(format:"%.2f" ,height  )) mts")
+                    }
+                    Slider(value:$height,in: 0...2.50).padding().accentColor(.green)
+                    HStack{
+                        Text("Weight").padding()
+                        Text("\(String(format:"%.0f",weight))kg")
+                    }
+                    Slider(value:$weight,in: 0...100).padding().accentColor(.green)
+                    
+                }.frame(maxWidth: .infinity,alignment: .leading)
+                Spacer()
+                Button(action:{calculate.toggle()},label:{
+                    Text("Calculate").padding()
+                })
+                //                NavigationLink(destination: CalculadoVista(), isActive: $calculate){
+                //                    EmptyView()
+                //                }.sheet(item: , onDismiss: <#T##(() -> Void)?##(() -> Void)?##() -> Void#>, content: <#T##(Identifiable) -> View#>)
+            }.sheet(isPresented: $calculate){
+                CalculadoVista(height:height,width: weight)
+            }
+            
+        }
+        
     }
-}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
